f7eeeb64174983f7ff324a5cf54c8972471a6fdb
bellCold <whdcks420@gmail.com>
2024-02-27 16:17:14 +0900

    fix: user service port const 처리

--- user-service/src/main/kotlin/event/userservice/api/auth/AuthController.kt
+++ user-service/src/main/kotlin/event/userservice/api/auth/AuthController.kt
@@ -1,10 +1,12 @@
 package event.userservice.api.auth
 
 import event.userservice.application.AuthService
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v1")
 class AuthController(private val authService: AuthService) {
 
     /**
@@ -13,4 +15,15 @@ class AuthController(private val authService: AuthService) {
      * logout
      * refresh
      */
+
+    @PostMapping("/login")
+    fun login(): String {
+        return "login"
+    }
+
+    @PostMapping("/logout")
+    fun logout(): String {
+        return "logout"
+    }
+
 }
