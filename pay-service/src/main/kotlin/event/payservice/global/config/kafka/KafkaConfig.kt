c8a68e2f8b9bcba3cdf77aa03f79db57253da535
bellCold <whdcks420@gmail.com>
2024-03-13 16:26:41 +0900

    fix: kafka 설정 수정

--- pay-service/src/main/kotlin/event/payservice/global/config/kafka/KafkaConfig.kt
+++ pay-service/src/main/kotlin/event/payservice/global/config/kafka/KafkaConfig.kt
@@ -1,12 +1,14 @@
 package event.payservice.global.config.kafka
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.listener.DefaultErrorHandler
 
 @Configuration
 class KafkaConfig(
@@ -20,9 +22,22 @@ class KafkaConfig(
     fun consumerFactory(): ConsumerFactory<String, Any> {
         val properties = mutableMapOf<String, Any>()
         properties[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "$kafkaServerHost:$kafkaServerPort"
-        properties[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
         properties[ConsumerConfig.GROUP_ID_CONFIG] = "consumerGroupId"
-        properties[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        properties[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        properties[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
         return DefaultKafkaConsumerFactory(properties)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String>? {
+        val kafkaListenerContainerFactory = ConcurrentKafkaListenerContainerFactory<String, String>()
+        kafkaListenerContainerFactory.consumerFactory = consumerFactory()
+        kafkaListenerContainerFactory.setCommonErrorHandler(errorHandler())
+        return kafkaListenerContainerFactory
+    }
+
+    @Bean
+    fun errorHandler(): DefaultErrorHandler {
+        return DefaultErrorHandler()
     }
 }
